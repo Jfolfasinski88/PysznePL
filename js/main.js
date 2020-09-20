a721da03982ed90e6f993973b5ba4572d70fdd82
unknown <KUBA>
2020-09-20 13:14:04 +0200

    dodanie email, adres, telefon sosy oraz napoje do order

--- js/main.js
+++ js/main.js
@@ -1,65 +1,45 @@
-// const mainBtn = document.getElementsByClassName('btn');
-// console.log("mainBtn button po klasie",mainBtn)
-// const input = document.getElementsByClassName('margin-bottom')
-// console.log("input po klasie", input)
-// const mainBtn = document.getElementsByClassName('btn');
-// console.log("mainBtn button po klasie", mainBtn)
-// const input = document.getElementsByClassName('margin-bottom');
-// console.log("input po klasie", input)
-
-// tworzy i dodaje nowe elementy do strony
-// const idBtn = document.getElementById('id-btn');
-// let btn = document.createElement("button");
-// btn.setAttribute('class', 'fun')
-// btn.innerText = "click me"
-// console.log(btn)
-// const maindiv = document.getElementById('main-div')
-// maindiv.appendChild(btn)
-// let span = document.createElement('span')
-// span.innerText = "<button> hi hi </button>"
-// let span2 = document.createElement('span')
-// span2.innerHTML = " hi hi jestem inner Html"
-// maindiv.appendChild(span)
-// maindiv.appendChild(span2)
-// idBtn.addEventListener("click", takeOrder)
-// function takeOrder() {
-//     sprawdzanieSosow("sosy")
-//     const pomidorowy = document.getElementById('pomidorowy')
-//     const czosnkowy = document.getElementById('czosnkowy')
-//     const ostryPomidorowy = document.getElementById('ostryPomidorowy')
-// }
-// console.log(btn)
-// function sprawdzanieSosow(nazwaKlasy) {
-//     const pobraneWszytkieSosyZinputu = document.getElementsByClassName(nazwaKlasy)
-//     for (let i = 0; i < pobraneWszytkieSosyZinputu.length; i++) {
-//         // console.log(pobraneWszytkieSosyZinputu[i])
-//         if (pobraneWszytkieSosyZinputu[i].checked === true) {
-//             console.log(pobraneWszytkieSosyZinputu[i].attributes)
-//         }
-//     }
-// }
-// function dodawanie(a, b) {
-//     let wynik = a + b
-//     console.log(wynik)
-// }
-// dodawanie(5, 15)
-// dodawanie(2, 15)
-// dodawanie(7, 1885)
-// dodawanie(5, 88)
-// dodawanie(7, 7)
-// 
-const idBtn = document.getElementById('id-btn')
-console.log("button po id",idBtn)
-
-idBtn.addEventListener("click",takeOrder)
-
+const idBtn = document.getElementById('id-btn');
+let order = {
+    pizza: '',
+    sosy: [],
+    name: '',
+    adress: '',
+    email: '',
+    tel: '',
+    text: '',
+    napoje: []
+}
+idBtn.addEventListener("click", takeOrder)
+function sprawdzaniecheckBox(nazwaKlasy) {
+    console.log(nazwaKlasy)
+    const pobraneWszytkieSosyZinputu = document.getElementsByClassName(nazwaKlasy)
+    if (nazwaKlasy === "sosy") {
+        for (let i = 0; i < pobraneWszytkieSosyZinputu.length; i++) {
+            order.sosy.push(pobraneWszytkieSosyZinputu[i].checked)
+        }
+    } else {
+        for (let i = 0; i < pobraneWszytkieSosyZinputu.length; i++) {
+            order.napoje.push(pobraneWszytkieSosyZinputu[i].checked)
+        }
+    }
+}
 function takeOrder() {
-    sprawdzanieSosow("sosy")
-    const pomidorowy = document.getElementById('pomidorowy')
-    console.log (pomidorowy.checked)
-    const czosnkowy = document.getElementById('czosnkowy')
-    console.log (czosnkowy.checked)
-    const ostry = document.getElementById('ostry')
-    console.log (ostry.checked)
-
+    sprawdzaniecheckBox("sosy")
+    sprawdzaniecheckBox("napoje")
+    // pobieram imię i nazwisko z input 
+    const nameC = document.getElementById('nameC').value
+    order.name = nameC
+    // pobieram email z input 
+    const email = document.getElementById('email').value
+    order.email = email
+    // pobieram telefon z input 
+    const tel = document.getElementById('tel').value
+    order.tel = tel
+    // pobieram adress
+    const adress = document.getElementById('adress')
+    order.adress = adress
+    // pobieram textarea 
+    const msg = document.getElementById('msg').value
+    order.text = msg
+    console.log("takeOrder -> nameC", order)
 }
